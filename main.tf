66d3711a19de34815f1b3f35f5aea2635aa213ef
BALABHASKARARAO <balabhaskararaoguntupalli@gmail.com>
2020-08-12 11:59:03 +0530

    main.tf edit-2

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_db_instance" "postgresql" {
   monitoring_interval             = var.monitoring_interval
   monitoring_role_arn             = var.monitoring_interval > 0 ? aws_iam_role.enhanced_monitoring.arn : ""
   deletion_protection             = var.deletion_protection
-  enabled_cloudwatch_logs_exports = var.cloudwatch_logs_exports
+#  enabled_cloudwatch_logs_exports = var.cloudwatch_logs_exports
 
   tags = merge(
     {
